98484041cd618632a5491eebe56c7901a1d2f8c2
Dmitry E. Kislov <kislov@easydan.com>
2016-01-03 13:03:23 +1000

    Added Issue: coauthor and main author

--- bp/aworker/models.py
+++ bp/aworker/models.py
@@ -99,11 +99,11 @@ class ArtExtra(models.Model):
     pass
 
 class Issue(models.Model):
-    authors = models.ManyToManyField(AbstractUserMixin, null=True, blank=True, verbose_name=_('Authors'))
+    coauthors = models.ManyToManyField(AbstractUserMixin, null=True, blank=True, verbose_name=_('Authors'), related_name='issues_all')
     created = models.DateTimeField(auto_now_add=True, verbose_name=_('Created'), blank=True, default=timezone.now())
     updated = models.DateTimeField(auto_now=True, verbose_name=_('Updated'), blank=True, default=timezone.now())
     paper = models.ForeignKey(Article, null=True, blank=True, verbose_name=_('Paper'))
-    
+    author = models.OneToOneField(AbstractUserMixin, blank=True, null=True, verbose_name=_('Main author'), related_name='issues')
 
 @python_2_unicode_compatible
 class Review(models.Model):
